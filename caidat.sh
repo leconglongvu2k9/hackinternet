a06c328d616d2cb5992e57f3b11fb7daa12d0578
VuLong <levu0013@gmail.com>
2024-09-10 19:36:30 +0700

    Update caidat.sh

--- caidat.sh
+++ caidat.sh
@@ -2,7 +2,7 @@ clear
 echo "Đang cài đặt"
 apt update && apt upgrade -y
 clear
-cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/leconglongvu2k9/hackinternet/raw/main/ver1.zip && unzip ver1.zip && chmod a+x neko && chmod a+x tun && chmod a+x psiphon-tunnel-core && chmod a+x qexit && chmod a+x wl
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/leconglongvu2k9/hackinternet/raw/main/beta.zip && unzip beta.zip && chmod a+x mi && chmod a+x on && chmod a+x psiphon-tunnel-core && chmod a+x off && chmod a+x nen
 clear
 cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/etc/ && pkg install wget -y && wget https://github.com/leconglongvu2k9/hackinternet/raw/main/lon && mv lon motd
 clear
